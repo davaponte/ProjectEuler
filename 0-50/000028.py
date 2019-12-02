a834f296ae3bf8ec88a2332b8557afc540519292
Dave <david.aponte@outlook.com>
2019-12-01 20:21:12 -0500

    Ligerísima modificación al 28 para ganar unas miserables milésimas

--- 0-50/000028.py
+++ 0-50/000028.py
@@ -2,25 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 #  000028.py
-#  
-#  Copyright 2019 data <data@ASUS-LAPTOP>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+#
 
 MaxC = 1001
 MaxR = 1001
@@ -45,17 +27,16 @@ while (k <= Maxk):
             do = Dir[2]
         elif (do == Dir[2]):
             do = Dir[3]
-        elif (do == Dir[3]):
+        else: # elif (do == Dir[3]):
             do = Dir[0]
 
 # ~ for r in range(MaxR):
     # ~ for c in range(MaxC):
         # ~ print('{0:^3}'.format(Matrix[r][c]), end=' ' )
     # ~ print()
-        
+
 Suma = -Matrix[ro][co]
 for r in range(MaxR):
-    Suma += Matrix[r][r]
-    Suma += Matrix[MaxR - r - 1][r]
+    Suma += Matrix[r][r] + Matrix[MaxR - r - 1][r]
 
 print('ANSWER: ', Suma)
