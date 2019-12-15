34aad86032dff2a8089a08bc9769d99bfc6d5889
Dave <david.aponte@outlook.com>
2019-12-15 12:03:30 -0500

    UltraFast version hecha en Python. No es mía

--- 000104.py
+++ 000104.py
@@ -9,11 +9,42 @@ def Fib():
         f0, f1 = f1, f1 + f0
         yield f1
 
+# def IsPandigital(s):
+#     l = list(s)
+#     l.sort()
+#     i = int(''.join(l))
+#     return 123456789 == i
+
 def IsPandigital(s):
-    l = list(s)
-    l.sort()
-    i = int(''.join(l))
-    return 123456789 == i
+    return set(s) == set('123456789')
+
+# rt5=5**.5
+# def check_first_digits(n):
+#     def mypow( x, n ):
+#         res=1.0
+#         for i in range(n):
+#             res *= x
+#             # truncation to avoid overflow:
+#             if res>1E20: res*=1E-10
+#         return res
+#     # this is an approximation for large n:
+#     F = mypow( (1+rt5)/2, n )/rt5
+#     s = '%f' % F
+#     if IsPandigital(s[:9]):
+#         print(n)
+#         return True
+#
+# a, b, n = 1, 1, 1
+# while True:
+#     if IsPandigital( str(a)[-9:] ):
+#         # Only when last digits are
+#         # pandigital check the first digits:
+#         if check_first_digits(n):
+#             break
+#     a, b = b, a+b
+#     b=b%1000000000
+#     n += 1
+
 
 n = 0
 for f in Fib():
